0a16e9648978b20a7a3625398d8c2df1ad794d19
obetancourthunicah <obetancourthunicah@gmail.com>
2024-03-05 21:16:28 -0600

    Detalles de presentacion y proteccion CFR

--- src/Controllers/Products/CategoryForm.php
+++ src/Controllers/Products/CategoryForm.php
@@ -10,10 +10,17 @@ class CategoryForm extends PublicController
     private $viewData = [];
     private $mode = "DSP";
 
+    private $crf_token = "";
+
     private $categoryId = 0;
     private $categoryName = "";
     private $categorySmallDesc = "";
     private $categoryStatus = "ACT";
+
+    private $isReadOnly = "readonly";
+
+    private $hasErrors = false;
+    private $errors = [];
 
     private $modeOptions = [
         "INS" => "Nueva Categoría",
@@ -29,9 +36,19 @@ class CategoryForm extends PublicController
         "DSC" => "Descontinuado"
     ];
 
+    private function throwError($message, $scope = "global")
+    {
+        $this->hasErrors = true;
+        error_log($message);
+        if (!isset($this->errors[$scope])) {
+            $this->errors[$scope] = [];
+        }
+        $this->errors[$scope][] = $message;
+    }
+
     private function cargar_datos()
     {
-        $this->categoryId = isset($_GET["category_id"]) ? $_GET["category_id"] : 0;
+        $this->categoryId = isset($_GET["category_id"]) ? intval($_GET["category_id"]) : 0;
         $this->mode = isset($_GET["mode"]) ? $_GET["mode"] : "DSP";
 
         if ($this->categoryId > 0) {
@@ -43,6 +60,100 @@ class CategoryForm extends PublicController
             }
         }
     }
+
+    private function getPostData()
+    {
+        if (!$this->validateCsfrToken()) {
+            $this->throwError("Error de aplicación, Token CSRF Inválido");
+        }
+        $tmp_mode = isset($_POST["mode"]) ? $_POST["mode"] : "DSP";
+        if ($tmp_mode !== $this->mode) {
+            $this->throwError("Error de aplicación, Modo de formulario incorrecto");
+        }
+        $tmp_category_id = isset($_POST["category_id"]) ? intval($_POST["category_id"]) : 0;
+        if ($this->mode === "INS") {
+            if ($tmp_category_id !== 0) {
+                $this->throwError("No se puede insertar con un valor de categoria", "category_id_error");
+            }
+        } else {
+            if ($tmp_category_id != $this->categoryId) {
+                $this->throwError("Error de Aplicación, no se puede modificar el valor del Identificador de la Categoria");
+            }
+        }
+        $this->categoryId = $tmp_category_id;
+
+        $tmp_category_name = isset($_POST["category_name"]) ? $_POST["category_name"] : "";
+        if (preg_match("/^\s*$/", $tmp_category_name)) {
+            $this->throwError("Debe ingresar el nombre de la categoria", "category_name_error");
+        }
+        if (!preg_match("/^[a-zA-Z0-9áéíóúüÁÉÍÓÚÜñÑ ]*$/", $tmp_category_name)) {
+            $this->throwError("El nombre de la categoria solo puede contener letras y números", "category_name_error");
+        }
+        $this->categoryName = $tmp_category_name;
+
+        $tmp_categorySmallDesc = isset($_POST["category_small_desc"]) ? $_POST["category_small_desc"] : "";
+        if (preg_match("/^\s*$/", $tmp_categorySmallDesc)) {
+            $this->throwError("Debe ingresar la descripción de la categoria", "category_small_desc_error");
+        }
+        if (!preg_match("/^[a-zA-Z0-9áéíóúüÁÉÍÓÚÜñÑ ]*$/", $tmp_category_name)) {
+            $this->throwError("La descripción de la categoria solo puede contener letras y números", "category_small_desc_error");
+        }
+        $this->categorySmallDesc = $tmp_categorySmallDesc;
+
+        $tmp_categoryStatus = isset($_POST["category_status"]) ? $_POST["category_status"] : "";
+        if (!isset($this->categoryStatusOptions[$tmp_categoryStatus])) {
+            $this->throwError("Debe seleccionar un estado para la categoria");
+        }
+        $this->categoryStatus = $tmp_categoryStatus;
+    }
+
+    private function processAction()
+    {
+        switch ($this->mode) {
+            case "INS":
+                $inserted = \Dao\Productos\Categories::insertCategory(
+                    $this->categoryName,
+                    $this->categorySmallDesc,
+                    $this->categoryStatus
+                );
+                if ($inserted) {
+                    \Utilities\Site::redirectToWithMsg(
+                        "index.php?page=Products_CategoriesList",
+                        "Categoria Guardada"
+                    );
+                } else {
+                    $this->throwError("Error al insertar la categoria");
+                }
+                break;
+            case "UPD":
+                $updated = \Dao\Productos\Categories::updateCategory(
+                    $this->categoryName,
+                    $this->categorySmallDesc,
+                    $this->categoryStatus,
+                    $this->categoryId
+                );
+                if ($updated) {
+                    \Utilities\Site::redirectToWithMsg(
+                        "index.php?page=Products_CategoriesList",
+                        "Categoria Actualizada"
+                    );
+                } else {
+                    $this->throwError("Error al actualizar la categoria");
+                }
+                break;
+            case "DEL":
+                $deleted = \Dao\Productos\Categories::deleteCategory($this->categoryId);
+                if ($deleted) {
+                    \Utilities\Site::redirectToWithMsg(
+                        "index.php?page=Products_CategoriesList",
+                        "Categoria Eliminada"
+                    );
+                } else {
+                    $this->throwError("Error al eliminar la categoria");
+                }
+                break;
+        }
+    }
     private function prepareViewData()
     {
         $viewData["mode"] = $this->mode;
@@ -52,6 +163,13 @@ class CategoryForm extends PublicController
         $viewData["category_small_desc"] = $this->categorySmallDesc;
         $viewData["category_status"] = $this->categoryStatus;
 
+        if ($this->mode === "INS" || $this->mode === "UPD") {
+            $this->isReadOnly = "";
+        }
+        $viewData["isReadOnly"] = $this->isReadOnly;
+
+        $viewData["showAction"] = $this->mode !== "DSP";
+
         foreach ($this->categoryStatusOptions as $value => $text) {
             $viewData["category_status_list"][] = [
                 "value" => $value,
@@ -60,8 +178,32 @@ class CategoryForm extends PublicController
             ];
         }
 
+        $this->crf_token = $this->csfrToken();
+        $viewData["crf_token"] = $this->crf_token;
+        $viewData["hasErrors"] = $this->hasErrors;
+        $viewData["errors"] = $this->errors;
+
         $this->viewData = $viewData;
     }
+
+    private function csfrToken()
+    {
+        $token = md5(uniqid(microtime(), true));
+        $_SESSION["category_form_token"] = $token;
+        return $token;
+    }
+
+    private function validateCsfrToken()
+    {
+        if (!isset($_POST["crf_token"])) {
+            return false;
+        }
+        if ($_POST["crf_token"] !== $_SESSION["category_form_token"]) {
+            return false;
+        }
+        return true;
+    }
+
     public function run(): void
     {
         /*
@@ -83,6 +225,14 @@ class CategoryForm extends PublicController
                     Mostrar Datos
         */
         $this->cargar_datos();
+
+        if ($this->isPostBack()) {
+            $this->getPostData();
+            if (!$this->hasErrors) {
+                $this->processAction();
+            }
+        }
+
         $this->prepareViewData();
         Renderer::render("productos/categoryform", $this->viewData);
     }
--- src/Views/templates/productos/categoryform.view.tpl
+++ src/Views/templates/productos/categoryform.view.tpl
@@ -1,37 +1,76 @@
 <h2>{{modeDesc}}</h2>
 <section>
-    <form action="index.php?page=Products_CategoryForm&mode={{mode}}&category_id={{category_id}}" method="post">
+    {{if hasErrors}}
+    <div class="error">
+        <ul>
+            {{with errors}}
+            {{foreach global}}
+            <li>{{this}}</li>
+            {{endfor global}}
+            {{endwith errors}}
+        </ul>
+    </div>
+    {{endif hasErrors}}
+    <form class="grid" action="index.php?page=Products_CategoryForm&mode={{mode}}&category_id={{category_id}}"
+        method="post">
         <input type="hidden" name="category_id" value="{{category_id}}">
+        <input type="hidden" name="mode" value="{{mode}}">
+        <input type="hidden" name="crf_token" value="{{crf_token}}">
         <div class="row">
-        <label for="category_id">ID</label>
-        <input type="text" name="category_id" id="category_id" value="{{category_id}}" disabled>
+            <label class="col-3 offset-2" for="category_id">ID</label>
+            <input class="col-5" type="text" name="category_id" id="category_id" value="{{category_id}}" disabled>
         </div>
         <div class="row">
-            <label for="category_name">Categoria</label>
-            <input type="text" name="category_name" id="category_name" value="{{category_name}}">
+            <label class="col-3 offset-2" for="category_name">Categoria</label>
+            <input class="col-5" type="text" {{isReadOnly}} name="category_name" id="category_name"
+                value="{{category_name}}">
+            {{if hasErrors}}
+            <div class="error col-5 offset-5">
+                {{with errors}}
+                {{foreach category_name_error}}
+                {{this}}<br />
+                {{endfor category_name_error}}
+                {{endwith errors}}
+            </div>
+            {{endif hasErrors}}
         </div>
         <div class="row">
-            <label for="category_small_desc">Descripcion</label>
-            <input type="text" name="category_small_desc" id="category_small_desc" value="{{category_small_desc}}">
+            <label class="col-3 offset-2" for="category_small_desc">Descripcion</label>
+            <input class="col-5" type="text" {{isReadOnly}} name="category_small_desc" id="category_small_desc"
+                value="{{category_small_desc}}">
+            {{if hasErrors}}
+            <div class="error col-5 offset-5">
+                {{with errors}}
+                {{foreach category_small_desc_error}}
+                {{this}}<br />
+                {{endfor category_small_desc_error}}
+                {{endwith errors}}
+            </div>
+            {{endif hasErrors}}
         </div>
         <div class="row">
-            <label for="category_status">Status</label>
-            <select id="category_status" name="category_status">
+            <label class="col-3 offset-2" for="category_status">Status</label>
+            <select class="col-5" id="category_status" name="category_status" {{if isReadOnly}} disabled readonly
+                {{endif isReadOnly}}>
                 {{foreach category_status_list}}
-                    <option value="{{value}}" {{selected}}>{{text}}</option>
+                <option value="{{value}}" {{selected}}>{{text}}</option>
                 {{endfor category_status_list}}
             </select>
         </div>
         <div class="row">
-            <button type="submit" name="btnGuardar" id="btnGuardar">Guaradar</button>
-            &nbsp;
-            <button name="btnCancelar" id="btnCancelar">Cancelar</button>
+            <div class="col-5 offset-5">
+                {{if showAction}}
+                <button type="submit" name="btnGuardar" id="btnGuardar">Guardar</button>
+                &nbsp;
+                {{endif showAction}}
+                <button name="btnCancelar" id="btnCancelar">Cancelar</button>
+            </div>
         </div>
 </section>
 <script>
-    document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function () {
         const btnCancelar = document.getElementById('btnCancelar');
-        btnCancelar.addEventListener('click', function(e) {
+        btnCancelar.addEventListener('click', function (e) {
             e.preventDefault();
             e.stopPropagation();
             window.location.href = 'index.php?page=Products_CategoriesList';
